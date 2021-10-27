5035e06a3d0854084d1c6af483d7ac30e8e32276
Jake <75119871+angelesjacob@users.noreply.github.com>
2021-10-27 10:17:51 +0200

    Add files via upload

---
+++ set 3 - Algorithms/plurality/plurality.c
@@ -0,0 +1,112 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // Set voted var to track if vote is valid or not
+    bool voted = false;
+
+    // Iterate and validate the name is a candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // if name is found, then increment vote by 1
+        if (candidates[i].name == name)
+        {
+            candidates[i].votes++;
+            voted = true;
+        }
+    }
+    return voted;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // Init winning vote count
+    int winningVotes = 0;
+
+    // Iterate and validate the votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // if name is found, then increment vote by 1
+        if (candidates[i].votes > winningVotes)
+        {
+            winningVotes = candidates[i].votes;
+        }
+    }
+
+    // Iterate and validate the votes
+    for (int j = 0; j < candidate_count; j++)
+    {
+        // if name is found, then increment vote by 1
+        if (candidates[j].votes == winningVotes)
+        {
+            printf("%s\n", candidates[j].name);
+        }
+    }
+    return;
+}
+
